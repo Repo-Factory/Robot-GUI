21a5374487b2e0b6887f8734090f80706bad7e20
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-10-05 22:08:57 -0700

    Make Stylesheet dynamic

--- gui_node/main/main.cpp
+++ gui_node/main/main.cpp
@@ -2,11 +2,15 @@
 #include <QApplication>
 #include <QFile>
 
+#ifndef STYLE_PATH          // Should be defined in CMakeLists.txt
+    #define STYLE_PATH ""
+#endif
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     QApplication a(argc, argv);
-    QFile styleFile("/home/robox/gui/server/gui_node/styles/Darkeum.qss");
+    QFile styleFile(STYLE_PATH);
     styleFile.open(QFile::ReadOnly);
     QString style(styleFile.readAll());
     a.setStyleSheet(style);
